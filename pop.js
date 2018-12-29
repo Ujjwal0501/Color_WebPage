6c27993f2bedc997b99612b0eed5d316e3b617cb
ujjwal0501 <31813384+Ujjwal0501@users.noreply.github.com>
2018-12-29 13:08:31 +0530

    add functionality to popup checkboc

--- pop.js
+++ pop.js
@@ -3,13 +3,37 @@ function applyChange(id) {
 	alert("hi!");
 }
 
+function changeColorInputState(state) {
+	document.getElementById('state').checked = !state;
+	var list1 = document.getElementsByClassName('container');
+	var val = "";
+	if (state) val = "none";
+	else val = "block";
+	list1[0].style.display = val;
+	list1[1].style.display = val;
+	list1[2].style.display = val;
+}
+
 window.onload = (function () {
 	document.getElementById('background').onchange = "applyChange('background')";
 	document.getElementById('text').onchange = "applyChange('text')";
 	document.getElementById('border').onchange = "applyChange('border')";
+	
+	document.getElementById('state').addEventListener('change', function () {
+		if (this.checked) {
+			localStorage.setItem('contrast-chrome-extension-state', 'enabled');
+			changeColorInputState(false);
+		} else {
+			localStorage.setItem('contrast-chrome-extension-state', 'disabled');
+			changeColorInputState(true);
+		}
+	});
+	
 	var result = localStorage.getItem('contrast-chrome-extension-state');
-	if (result == "enabled")
-		document.getElementById('state').checked = true;
-	else
-		document.getElementById('state').checked = false;
+	if (result == "enabled") {
+		changeColorInputState(false);
+	}
+	else {
+		changeColorInputState(true);
+	}
 });
